5c0306b03d635a4a6b2ecf913431544cb7afe9ef
Ludwig Van <ludwig.eugenio@directwithhotels.com>
2020-10-01 21:41:32 +0800

    Updates

---
+++ precache-manifest.eabd31420b72d6e5359b23842103ccc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e688af059ac9f291e06fe10d0b3a470d",
+    "url": "/vetz/index.html"
+  },
+  {
+    "revision": "587d38edb921b922c409",
+    "url": "/vetz/static/css/main.29ac71af.chunk.css"
+  },
+  {
+    "revision": "5ff636b5b07f6e254fef",
+    "url": "/vetz/static/js/2.0ac5e0b4.chunk.js"
+  },
+  {
+    "revision": "a75ee770eeb065faa45ae4929f4eae76",
+    "url": "/vetz/static/js/2.0ac5e0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587d38edb921b922c409",
+    "url": "/vetz/static/js/main.fb0f83a3.chunk.js"
+  },
+  {
+    "revision": "efcad73668a8a1dc6767",
+    "url": "/vetz/static/js/runtime-main.0e14d5aa.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/vetz/static/media/OpenSansRegular.3ed9575d.ttf"
+  }
+]);
